4e2e43556bf56e6bf3ee4d958150d5816048a018
Barnamay Chowdhury <barnamay1234@gmail.com>
2025-07-09 17:41:45 +0530

    fix: rename authoptions to authOptions (camelCase)

--- app/api/events/unapproved/route.js
+++ app/api/events/unapproved/route.js
@@ -1,10 +1,10 @@
 import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
-import { authoptions } from '@/app/api/auth/[...nextauth]/options'
+import { authOptions } from '@/app/api/auth/[...nextauth]/options'
 
 export async function GET(req) {
-  const session = await getServerSession(authoptions)
+  const session = await getServerSession(authOptions)
 
   if (!session || session.user.role !== 'admin') {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
